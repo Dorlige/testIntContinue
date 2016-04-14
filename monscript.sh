b38a0da835ebe4d9b9986ed032eccef8c7f5e53c
Dorlige <saindy.nkadi@yahoo.fr>
2016-04-14 04:46:00 +0200

    Update monscript.sh

--- monscript.sh
+++ monscript.sh
@@ -10,19 +10,19 @@ git config --global user.name "Dorlige"
 
 
 #clone branch gh-pages
-#git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${GH_REF}  gh-pages > /dev/null
-git clone --quiet https://${GH_TOKEN}@github.com/Dorlige/testIntContinue.git gh-pages > /dev/null
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${GH_REF}  gh-pages > /dev/null
+#git clone --quiet https://${GH_TOKEN}@github.com/Dorlige/testIntContinue.git gh-pages > /dev/null
 
 cd gh-pages
 ls
 #cd master
 cp $HOME/html/faq.html ./index.html
 
-ls -al index.html
+
 
  #add, commit and push files
- git add -f index.html
- git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages "
+ git add -f .
+# git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages "
  #git push -fq origin gh-pages > /dev/null
  #git push -fq origin master > /dev/null
 
